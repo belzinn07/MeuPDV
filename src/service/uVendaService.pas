0b2be716781ca699f7eb7f8c008815cd806af11a
Dmorim <netobelmiro1@gmail.com>
2026-09-05 10:17:30 -0300

    feat: extrai a validação de itens e o cálculo de total para métodos privados

--- src/service/uVendaService.pas
+++ src/service/uVendaService.pas
@@ -1,4 +1,4 @@
-unit uVendaService;
+﻿unit uVendaService;
 
 interface
 
@@ -17,6 +17,8 @@ type
   private
     FRepository : IVendaRepository;
     FValidadorVenda : IValidador<TVendaDTO>;
+    procedure ValidarItens(const AVendaDTO: TVendaDTO; var ItemDTO: TItemVendaDTO);
+    procedure CalcularTotal(const AVendaDTO: TVendaDTO; var ItemDTO: TItemVendaDTO; var Total: Currency);
 
   public
     constructor Create(ARepository: IVendaRepository);
@@ -43,34 +45,16 @@ end;
 
 procedure TVendaService.Salvar(const AVendaDTO: TVendaDTO);
 var
-  ItemValidador : IValidador<TItemVendaDTO>;
   ItemDTO : TItemVendaDTO;
   Item : TItemVenda;
   Itens : TObjectList<TItemVenda>;
   Venda : TVenda;
   Total : Currency;
-  ItemAtual : Integer;
 
 begin
   FValidadorVenda.Validar(AVendaDTO);
-
-  ItemValidador := TItemVendaValidador.Create;
-  ItemAtual := 0;
-  for ItemDTO in AVendaDTO.Itens do
-  begin
-    Inc(ItemAtual);
-    try
-      ItemValidador.Validar(ItemDTO);
-    except
-      raise Exception.CreateFmt('Item %d da venda inválido: %s',
-        [ItemAtual, Exception(ExceptObject).Message]);
-    end;
-  end;
-
-  Total := 0;
-  for ItemDTO in AVendaDTO.Itens do
-    Total := Total +
-      (StrToInt(ItemDTO.Quantidade) * StrToFloat(ItemDTO.ValorUnitario));
+  ValidarItens(AVendaDTO, ItemDTO);
+  CalcularTotal(AVendaDTO, ItemDTO, Total);
 
   AVendaDTO.Data := Now;
   AVendaDTO.Total := Total;
@@ -96,6 +80,34 @@ begin
     begin
       raise Exception.CreateFmt(
         'Erro ao salvar venda, nenhum dado foi gravado: %s', [E.Message]);
+    end;
+  end;
+end;
+
+procedure TVendaService.CalcularTotal(const AVendaDTO: TVendaDTO; var ItemDTO: TItemVendaDTO; var Total: Currency);
+var
+  Local_ItemDTO: TItemVendaDTO;
+begin
+  Total := 0;
+  for Local_ItemDTO in AVendaDTO.Itens do
+    Total := Total + (StrToInt(Local_ItemDTO.Quantidade) * StrToFloat(Local_ItemDTO.ValorUnitario));
+end;
+
+procedure TVendaService.ValidarItens(const AVendaDTO: TVendaDTO; var ItemDTO: TItemVendaDTO);
+var
+  ItemValidador: uIValidador.IValidador<TItemVendaDTO>;
+  ItemAtual: Integer;
+  Local_ItemDTO: TItemVendaDTO;
+begin
+  ItemValidador := TItemVendaValidador.Create;
+  ItemAtual := 0;
+  for Local_ItemDTO in AVendaDTO.Itens do
+  begin
+    Inc(ItemAtual);
+    try
+      ItemValidador.Validar(Local_ItemDTO);
+    except
+      raise Exception.CreateFmt('Item %d da venda inválido: %s', [ItemAtual, Exception(ExceptObject).Message]);
     end;
   end;
 end;
